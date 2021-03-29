c217caa22485444826cab29e04de9d0e1a7c2acf
Mehul Patel <mehulp1123@gmail.com>
2021-03-29 13:29:01 -0400

    added dev tools

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = (): JSX.Element => {
+	return (
+		<div>Hello World!</div>
+	);
+};
+
+export default App;
---
+++ webpack.config.ts
@@ -0,0 +1,48 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import {Configuration as WebpackDevServerConfiguration} from "webpack-dev-server";
+import {Configuration as WebpackConfiguration} from "webpack";
+
+interface Configuration extends WebpackConfiguration {
+	devServer?: WebpackDevServerConfiguration;
+}
+
+const config: Configuration = {
+	mode: "development",
+	context: path.resolve(__dirname, "src"),
+	entry: "./index.tsx",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "main.js",
+		clean: true
+	},
+	devServer: {
+		host: "localhost",
+		liveReload: true,
+		port: 3000,
+		contentBase: path.join(__dirname, "dist")
+	},
+	resolve: {
+		extensions: [".tsx", ".ts", ".jsx", ".js"]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"]
+			},
+			{
+				test: /\.tsx?$/,
+				use: "ts-loader",
+				exclude: /node_modules/
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "src", "index.html")
+		})
+	]
+};
+
+export default config;
