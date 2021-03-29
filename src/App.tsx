250386a50c9228e2b8f79182e0b010de2a114af7
Mehul Patel <mehulp1123@gmail.com>
2021-03-29 13:49:00 -0400

    checking if husky hook works

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const App = (): JSX.Element => {
-	return (
-		<div>Hello World!</div>
-	);
+	return <div>Hello World!</div>;
 };
 
 export default App;
