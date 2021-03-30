e2ad4f7038c0fc8cc647c098855933f3783f10b5
Mehul Patel <mehulp1123@gmail.com>
2021-03-30 13:51:07 -0400

    added scroll effect to name component

--- src/components/Name.tsx
+++ src/components/Name.tsx
@@ -1,13 +1,46 @@
-import React from "react";
+import React, {useLayoutEffect, useRef} from "react";
 
 const Name = (): JSX.Element => {
+	// const [hide, setHide] = useState(false);
+
+	const topPos = (element: HTMLElement) => {
+		if (element === null) return 0;
+		return element.getBoundingClientRect().top;
+	};
+
+	const nameRef = useRef<HTMLDivElement>(null);
+
+	useLayoutEffect(() => {
+		const onScroll = () => {
+			const nameContainerPos = topPos(nameRef.current as HTMLElement);
+			if (nameContainerPos <= 0) {
+				const nameContainer = nameRef.current;
+				if (nameContainer) {
+					nameContainer.classList.add("name-scroll", "sticky-logo");
+					// setHide(true);
+				}
+			} else if (nameContainerPos > 0) {
+				const nameContainer = nameRef.current;
+				if (nameContainer) {
+					nameContainer.classList.remove("name-scroll", "sticky-logo");
+					// setHide(false);
+				}
+			}
+		};
+
+		window.addEventListener("scroll", onScroll);
+		return () => window.removeEventListener("scroll", onScroll);
+	}, []);
+
 	return (
-		<div className='name-container'>
+		<div ref={nameRef} className='name-container'>
 			<div className='firstname'>
 				<span className='first-letter'>M</span>ehul
+				{/* {!hide && <span className='name-shadow first-name-shadow'>M</span>} */}
 			</div>
 			<div className='lastname'>
 				<span className='first-letter'>P</span>atel
+				{/* {!hide && <span className='name-shadow'>P</span>} */}
 			</div>
 		</div>
 	);
