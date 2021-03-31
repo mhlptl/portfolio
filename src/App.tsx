954fb14ff7d917bf5f7b207a3a9a4545063d038e
Mehul Patel <mehulp1123@gmail.com>
2021-03-30 21:45:16 -0400

    created skills page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Name from "./components/Name";
 import NavBar from "./components/NavBar";
 import SlantedBox from "./components/SlantedBox";
+import SkillsPage from "./pages/SkillsPage";
 
 const App = (): JSX.Element => {
 	return (
@@ -12,6 +13,7 @@ const App = (): JSX.Element => {
 			<SlantedBox containerClassName={"slanted-div-under-container"} className={"slanted-div-under"} />
 			<SlantedBox containerClassName={"slanted-div-under-container2"} className={"slanted-div-under2"} />
 			<SlantedBox containerClassName={"slanted-div-under-container3"} className={"slanted-div-under3"} />
+			<SkillsPage />
 		</React.Fragment>
 	);
 };
