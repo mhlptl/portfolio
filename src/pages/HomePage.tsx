b58a1d688da3f6541f9b44476c203bc60d90c126
Mehul Patel <mehulp1123@gmail.com>
2021-04-03 16:43:50 -0400

    made top slanted box responsive

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Footer from "../components/Footer";
+// import Footer from "../components/Footer";
 import Name from "../components/Name";
 import SlantedBox from "../components/SlantedBox";
-import ExperiencePage from "./ExperiencePage";
-import ProjectsPage from "./ProjectsPage";
-import SkillsPage from "./SkillsPage";
+// import ExperiencePage from "./ExperiencePage";
+// import ProjectsPage from "./ProjectsPage";
+// import SkillsPage from "./SkillsPage";
 
 const HomePage = (): JSX.Element => {
 	return (
@@ -14,10 +14,10 @@ const HomePage = (): JSX.Element => {
 			<SlantedBox containerClassName={"slanted-div-under-container"} className={"slanted-div-under"} />
 			<SlantedBox containerClassName={"slanted-div-under-container2"} className={"slanted-div-under2"} />
 			<SlantedBox containerClassName={"slanted-div-under-container3"} className={"slanted-div-under3"} />
-			<ExperiencePage />
+			{/* <ExperiencePage />
 			<SkillsPage />
 			<ProjectsPage />
-			<Footer />
+			<Footer /> */}
 		</div>
 	);
 };
