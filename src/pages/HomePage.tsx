79582ea0ec74d360567cba8b5d04845a94bc1610
Mehul Patel <mehulp1123@gmail.com>
2021-04-02 23:51:51 -0400

    added experience section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from "../components/Footer";
 import Name from "../components/Name";
 import SlantedBox from "../components/SlantedBox";
+import ExperiencePage from "./ExperiencePage";
 import ProjectsPage from "./ProjectsPage";
 import SkillsPage from "./SkillsPage";
 
@@ -13,6 +14,7 @@ const HomePage = (): JSX.Element => {
 			<SlantedBox containerClassName={"slanted-div-under-container"} className={"slanted-div-under"} />
 			<SlantedBox containerClassName={"slanted-div-under-container2"} className={"slanted-div-under2"} />
 			<SlantedBox containerClassName={"slanted-div-under-container3"} className={"slanted-div-under3"} />
+			<ExperiencePage />
 			<SkillsPage />
 			<ProjectsPage />
 			<Footer />
