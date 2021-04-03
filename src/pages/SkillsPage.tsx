3c9b63bfaee9d9ae69cb9e1bd79687f1679cd9cf
Mehul Patel <mehulp1123@gmail.com>
2021-04-02 22:17:00 -0400

    changed design for skills + added different images

--- src/pages/SkillsPage.tsx
+++ src/pages/SkillsPage.tsx
@@ -13,9 +13,9 @@ import PGSVG from "../svg/postgres.svg";
 import PySVG from "../svg/python.svg";
 import JavaSVG from "../svg/java.svg";
 import Skill from "../components/Skill";
-import Webdev from "../images/webdev.jpg";
-import Cloud from "../images/cloud.jpg";
-import Automation from "../images/automation.png";
+import Webdev from "../images/webdev.svg";
+import Cloud from "../images/server.svg";
+import Automation from "../images/code.svg";
 
 const SkillsPage = (): JSX.Element => {
 	return (
@@ -23,61 +23,73 @@ const SkillsPage = (): JSX.Element => {
 			<div className='section-container'>
 				<Header className={"section-title"} word={"Skills"} />
 				<div className='inner-section'>
-					<div className='left'>
-						<h2 className='section-subtitle'>Full Stack Development</h2>
-						<div className='skills-container'>
-							<Skill SVG={HTMLSVG} name='HTML' />
-							<Skill SVG={CSSSVG} name='CSS' />
-							<Skill SVG={JSSVG} name='JavaScript' />
-							<Skill SVG={TSSVG} name='TypeScript' />
-							<Skill SVG={ReactSVG} name='ReactJS' />
-							<Skill SVG={NodeSVG} name='NodeJS' />
-						</div>
+					<div className='section-description'>
 						<div>
-							<ul className='section-list'>
-								<li>Building responsive frontends using React.js</li>
-								<li>Creating backends for applications using Node.js</li>
-								<li>TypeScript to enhance code quality</li>
-							</ul>
+							<h2 className='section-subtitle'>Full Stack Development</h2>
+							<div className='skills-container'>
+								<Skill SVG={HTMLSVG} name='HTML' />
+								<Skill SVG={CSSSVG} name='CSS' />
+								<Skill SVG={JSSVG} name='JavaScript' />
+								<Skill SVG={TSSVG} name='TypeScript' />
+								<Skill SVG={ReactSVG} name='ReactJS' />
+								<Skill SVG={NodeSVG} name='NodeJS' />
+							</div>
+							<div>
+								<ul className='section-list'>
+									<li>Building responsive frontends using React.js</li>
+									<li>Creating backends for applications using Node.js</li>
+									<li>TypeScript to enhance code quality</li>
+								</ul>
+							</div>
 						</div>
 					</div>
-					<img src={Webdev} className={"skills-image"} />
-				</div>
-
-				<div className='inner-section'>
-					<img src={Cloud} className={"skills-image"} />
-					<div className='right'>
-						<h2 className='section-subtitle'>Cloud</h2>
-						<div className='skills-container'>
-							<Skill SVG={DockerSVG} name='Docker' />
-							<Skill SVG={AWSSVG} name='Amazon Web Services' />
-							<Skill SVG={MongoSVG} name='MongoDB' />
-							<Skill SVG={PGSVG} name='PostgreSQL' />
-						</div>
-						<div>
-							<ul className='section-list'>
-								<li>Using Postgres + MongoDB to store data</li>
-								<li>Containerizing projects using Docker for seamless deployment</li>
-							</ul>
-						</div>
+					<div className='section-image-container'>
+						<Webdev className={"skills-image"} />
 					</div>
 				</div>
 
 				<div className='inner-section'>
-					<div className='left'>
-						<h2 className='section-subtitle'>Other</h2>
-						<div className='skills-container'>
-							<Skill SVG={PySVG} name='Python' />
-							<Skill SVG={JavaSVG} name='Java' />
-						</div>
+					<div className='section-image-container'>
+						<Cloud className={"skills-image"} />
+					</div>
+					<div className='section-description'>
 						<div>
-							<ul className='section-list'>
-								<li>Running automation scripts using Python</li>
-								<li>Building GUI applications using Java and JavaFX</li>
-							</ul>
+							<h2 className='section-subtitle'>Cloud</h2>
+							<div className='skills-container'>
+								<Skill SVG={DockerSVG} name='Docker' />
+								<Skill SVG={AWSSVG} name='Amazon Web Services' />
+								<Skill SVG={MongoSVG} name='MongoDB' />
+								<Skill SVG={PGSVG} name='PostgreSQL' />
+							</div>
+							<div>
+								<ul className='section-list'>
+									<li>Using Postgres + MongoDB to store data</li>
+									<li>Containerizing projects using Docker for seamless deployment</li>
+								</ul>
+							</div>
 						</div>
 					</div>
-					<img src={Automation} className='skills-image transparent' />
+				</div>
+
+				<div className='inner-section'>
+					<div className='section-description'>
+						<div>
+							<h2 className='section-subtitle'>Other</h2>
+							<div className='skills-container'>
+								<Skill SVG={PySVG} name='Python' />
+								<Skill SVG={JavaSVG} name='Java' />
+							</div>
+							<div>
+								<ul className='section-list'>
+									<li>Running automation scripts using Python</li>
+									<li>Building GUI applications using Java and JavaFX</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div className='section-image-container'>
+						<Automation className='skills-image' />
+					</div>
 				</div>
 			</div>
 		</div>
